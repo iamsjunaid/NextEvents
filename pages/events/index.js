8bfc76c2abacd2f8132017b66e45a4a7ed11e2f5
Junaid Syed <sjunaid626@gmail.com>
2024-07-10 18:19:55 +0530

    Update index.js

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,25 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+
+import { getAllEvents } from "../../dummy-data";
+
+import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
+
 export default function EventsPage() {
+  const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventsHandler(year, month) {
+    const path = `/events/${year}/${month}`;
+
+    router.push(path);
+  }
+
   return (
-    <div>
-      <h1>Events Page</h1>
-    </div>
+    <Fragment>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </Fragment>
   );
 }
