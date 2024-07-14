473b10bbe3e59e434284a7e89caa18dade799e2e
Junaid Syed <sjunaid626@gmail.com>
2024-07-14 12:47:16 +0530

    Update main-header.js

--- components/layout/main-header.js
+++ components/layout/main-header.js
@@ -1,21 +1,20 @@
-import Link from "next/link";
-import classes from "./main-header.module.css";
+import Link from 'next/link';
+
+import classes from './main-header.module.css';
 
 function MainHeader() {
   return (
     <header className={classes.header}>
       <div className={classes.logo}>
-        <Link href="/">NextEvents</Link>
+        <Link href='/'>NextEvents</Link>
       </div>
-      <main>
-        <nav className={classes.navigation}>
-          <ul>
-            <li>
-              <Link href="/events">Browse All Events</Link>
-            </li>
-          </ul>
-        </nav>
-      </main>
+      <nav className={classes.navigation}>
+        <ul>
+          <li>
+            <Link href='/events'>Browse All Events</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
