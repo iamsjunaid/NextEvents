6d63050c55b082b7be742a7a16eefa08b9c117e6
Junaid Syed <sjunaid626@gmail.com>
2024-07-15 13:47:56 +0530

    Create newsletter-registration.js

---
+++ components/input/newsletter-registration.js
@@ -0,0 +1,43 @@
+import classes from './newsletter-registration.module.css';
+import {useRef} from 'react';
+
+function NewsletterRegistration() {
+  const emailInputRef = useRef();
+
+  function registrationHandler(event) {
+    event.preventDefault();
+
+    // fetch user input (state or refs)
+    const enteredEmail = emailInputRef.current.value;
+    // optional: validate input
+
+    // send valid data to API
+    fetch('/api/newsletter', {
+      method: 'POST',
+      body: JSON.stringify({ email: enteredEmail }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  return (
+    <section className={classes.newsletter}>
+      <h2>Sign up to stay updated!</h2>
+      <form onSubmit={registrationHandler}>
+        <div className={classes.control}>
+          <input
+            type='email'
+            id='email'
+            placeholder='Your email'
+            aria-label='Your email'
+            ref={emailInputRef}
+          />
+          <button>Register</button>
+        </div>
+      </form>
+    </section>
+  );
+}
+
+export default NewsletterRegistration;
