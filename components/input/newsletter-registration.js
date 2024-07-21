9e18ecd11eb272dab676761aa3535a1a8b4d29f0
Junaid Syed <sjunaid626@gmail.com>
2024-07-21 19:09:50 +0530

    Update newsletter-registration.js

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,24 +1,53 @@
-import classes from './newsletter-registration.module.css';
-import {useRef} from 'react';
+import { useRef, useContext } from "react";
+
+import classes from "./newsletter-registration.module.css";
+import NotificationContext from "../../store/notification-context";
 
 function NewsletterRegistration() {
   const emailInputRef = useRef();
+  const notificationCtx = useContext(NotificationContext);
 
   function registrationHandler(event) {
     event.preventDefault();
 
-    // fetch user input (state or refs)
     const enteredEmail = emailInputRef.current.value;
-    // optional: validate input
 
-    // send valid data to API
-    fetch('/api/newsletter', {
-      method: 'POST',
+    notificationCtx.showNotification({
+      title: "Signing up...",
+      message: "Registering for newsletter.",
+      status: "pending",
+    });
+
+    fetch("/api/newsletter", {
+      method: "POST",
       body: JSON.stringify({ email: enteredEmail }),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-    });
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+
+        return response.json().then((data) => {
+          throw new Error(data.message || "Something went wrong!");
+        });
+      })
+      .then((data) => {
+        notificationCtx.showNotification({
+          title: "Success!",
+          message: "Successfully registered for newsletter!",
+          status: "success",
+        });
+      })
+      .catch((error) => {
+        notificationCtx.showNotification({
+          title: "Error!",
+          message: error.message || "Something went wrong!",
+          status: "error",
+        });
+      });
   }
 
   return (
@@ -27,10 +56,10 @@ function NewsletterRegistration() {
       <form onSubmit={registrationHandler}>
         <div className={classes.control}>
           <input
-            type='email'
-            id='email'
-            placeholder='Your email'
-            aria-label='Your email'
+            type="email"
+            id="email"
+            placeholder="Your email"
+            aria-label="Your email"
             ref={emailInputRef}
           />
           <button>Register</button>
